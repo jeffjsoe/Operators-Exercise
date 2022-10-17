0dc7ab12bf62611fb60641fc11f8c4d449e3eb1b
Jeffrey Jose <jefjosecool@gmail.com>
2022-10-17 15:15:47 -0400

    Finished exercise 2 by the grace of GOD

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            //-------------EXERCISE 1-------------------
+            //If we declare a variable we can use it for whatever purpose we want later in the code, so we dont have to constantly write 17 or 4. Instead we can assign a value and declare it earlier in the code. 
             var a = 17;
             var b = 4;
 
@@ -21,7 +23,17 @@
             var remainder=a % b;
             //Here we plug in the declartion of the variables a and b and insert into a string
             Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            //------------EXERCISE 2---------------
+            //This takes raduis as a string and convers it into a double and stores it in radius
+            //-Make sure to write a desrption so the user doesnt get confused on what your asking for it to input for
+            Console.WriteLine("What is the radius of your circle");
+            var radius = double.Parse(Console.ReadLine());
+            //--Make sure to solve it- 
+            //-Here what it does is that it takes pi and multplies it by the power thingy. Which for example if radius is 4 it will be somethibng like var radius==4, then it will be (radius,2)--WHich is essentaly 4 to the power of 2*the math pi which is pi from the math library. 
+            var answer = Math.PI * Math.Pow(radius, 2);
 
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {answer}");
+            //-------------------Exercise 2----------------
 
         }
     }
