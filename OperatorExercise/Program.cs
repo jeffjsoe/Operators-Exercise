cb719291c4b1877716a907383cce8e3fbcf60030
Jeffrey Jose <jefjosecool@gmail.com>
2022-10-17 14:58:03 -0400

    Finished Exercise 1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 17;
+            var b = 4;
+
+            var sum = a += b;
+            Console.WriteLine(sum);
+
+            var diff=a-=b;
+            Console.WriteLine(diff);
+            //--a=a*b
+            var product = a *= b;
+            Console.WriteLine(product);
+            //--a=a/b
+            var quotient = a /= b;
+
+            var remainder=a % b;
+            //Here we plug in the declartion of the variables a and b and insert into a string
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+
         }
     }
 }
